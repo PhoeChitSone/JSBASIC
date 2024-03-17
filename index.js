e861328289df477e2b1ca49d087095c628e88c8f
PhoeChitSone <pcsone1998@gmail.com>
2024-03-17 13:46:31 +0630

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-// string 
-console.log("Hello world");
-// number
-console.log(12.17);
-// Boolean
-console.log(true);
-console.log(false);
-console.log("hello");
+// 
+let x = 10;
+// x = x+2;
+// x +=2;
+// x = x-2;
+// x -= 2;
 
+//x++
+//x--
 
+//console.log(x);
 
+// let myName = "Phoe Chit Sone";
 
+// if (myName == "Phoe Chit Sone"){
+//     console.log("Your name is Phoe Chit Sone");
+// }else{
+//     console.log("Your name is not Phoe Chit Sone");
+// }
 
-// variable
+// falsey 6 
 
-var name="Aung Aung";
+// 1 false
+// 2 0(zero)
+// 3 '' or "" (empty string)
+// 4 null
+// 5 undefined
+// 6 Not a number (NaN)
