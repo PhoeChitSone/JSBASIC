aa9cffb9d2af803fa057536d9f03f1b437275a2e
PhoeChitSone <pcsone1998@gmail.com>
2024-03-20 21:06:49 +0630

    adding loop

--- index.js
+++ index.js
@@ -26,3 +26,82 @@ let x = 10;
 // 4 null
 // 5 undefined
 // 6 Not a number (NaN)
+
+// function
+// function declaration
+
+// function showName(name){
+//     alert(name);
+// }
+
+// showName("Phoe Chit Sone");
+
+// function expression
+
+// var showAge = function(name,age){
+//     console.log(name,age);
+// }
+// showAge("Phoe Chit Sone",26);
+
+// function helloWorldTxt(){
+//     return "Hello World";
+// }
+// console.log(helloWorldTxt());
+
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+// let result = sum(39,40);
+// console.log(result);
+
+// es6 version syntax
+
+// es5 
+
+// var showAge = function(name,age){
+//         console.log(name,age);
+//     }
+//     showAge("Phoe Chit Sone",26);
+
+
+// // es 6
+
+//  let showMyAge = (name,age)=>{
+//     console.log(name,age);
+// }
+// showMyAge("Phoe Chit Sone",25);
+
+// Array
+
+// let fruits = ["apple", "orange", "mango"];
+// console.log(fruits.length);
+
+// let myBio = [];
+
+// let insertDataTomyBioArray = (name,age,job)=>{
+//     myBio.push(name);
+//     myBio.push(age);
+//     myBio.push(job);
+// }
+// insertDataTomyBioArray("Phoe Chit Sone",26,"Junior Programmer");
+// console.log(myBio);
+
+// for (let i=6;i>=1;i--){
+//     console.log("Hello world "+i);
+// }
+
+// loop 
+
+//for loop
+let fruits=["apple","banana","mango","lemon"];
+
+// for(let i =0;i<fruits.length;i++){
+//     console.log("My favorite fruit is "+fruits[i]);
+// }
+
+// while loop
+let i =0;
+while(i<fruits.length){
+    console.log("My favorite fruit is "+fruits[i]);
+    i++;
+}
